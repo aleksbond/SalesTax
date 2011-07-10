7fb67e2a857dc5317147a88be69b18422498185e
Aleksandra Bondzic <abondzic@thoughtworks.com>
2011-07-10 08:17:14 -0500

    Finally got the proper rounding for taxes figured out, calculating sales tax and import tax completed, now to make function that will do both

--- src/CalculateTaxes.java
+++ src/CalculateTaxes.java
@@ -1,16 +1,27 @@
-
+import java.text.DecimalFormat;
 
 public class CalculateTaxes {
 
-    private static double SALESTAX = .1;
-    private static double ROUND = .5;
+    private static double SALES_TAX = .1;
+    private static double IMPORTED_TAX = .05;
+    private static double ROUND_INT = .5;
+    private static double HUNDRED = 100.0;
+    private static double ROUNDING_DECIMAL = .05;
+
 
     public double calculateOnlySalesTax(double price) {
-        int priceInPennies = (int)(price*100.0);
-        int taxesInPennies = (int)(priceInPennies*SALESTAX + ROUND);
-        double taxes = ((double)taxesInPennies)/100;
-        return taxes;
+        int priceInPennies = (int)(price*HUNDRED);
+        int taxesInPennies = (int)(priceInPennies*SALES_TAX + ROUND_INT);
+        double taxes = ((double)taxesInPennies)/HUNDRED;
+        double roundedTaxes = Math.ceil(taxes/ROUNDING_DECIMAL)*ROUNDING_DECIMAL;
+        return Math.floor(roundedTaxes*HUNDRED)/HUNDRED;
     }
-
+    public double calculateOnlyImportedTax(double price) {
+        int priceInPennies = (int)(price*HUNDRED);
+        int taxesInPennies = (int)(priceInPennies*IMPORTED_TAX + ROUND_INT);
+        double taxes = ((double)taxesInPennies)/HUNDRED;
+        double roundedTaxes = Math.ceil(taxes/ROUNDING_DECIMAL)*ROUNDING_DECIMAL;
+        return Math.floor(roundedTaxes*HUNDRED)/HUNDRED;
+    }
 
 }
--- src/CalculateTaxesTest.java
+++ src/CalculateTaxesTest.java
@@ -8,6 +8,19 @@ public class CalculateTaxesTest extends CalculateTaxes{
     @Test
     public void testCalculateOnlySalesTax() throws  Exception{
         assertThat(calculateOnlySalesTax(14.99), is(1.5));
+        assertThat(calculateOnlySalesTax(18.99), is(1.9));
+        assertThat(calculateOnlySalesTax(10.00), is(1.0));
+    }
+
+    @Test
+    public void testCalculateOnlyImportedTax() throws Exception{
+        assertThat(calculateOnlyImportedTax(11.25), is(.6));
+        assertThat(calculateOnlyImportedTax(10.00), is(.5));
+    }
+
+    @Test
+    public void testCalculateSalesAndImportedTax() throws Exception{
+        //assertThat(calculateSalesAndImportedTax(), is());
     }
 
 }
